8b7507f076eef4ca22322289c19551d7d4a7370d
Spence Southard <Spence.Southard@gmail.com>
2015-04-14 13:30:29 -0400

    Updated color palette

--- haiti/about.xaml.cs
+++ haiti/about.xaml.cs
@@ -10,32 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Diagnostics;
 
 namespace haiti
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for about.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class about : Window
     {
-        public MainWindow()
+        public about()
         {
             InitializeComponent();
-            //Process.Start("C:\\Olltwit\\Match Up Extra\\matchupextra.exe");
         }
 
         public void buttonClick(object sender, RoutedEventArgs e)
         {
-            string name = (string)((Button)sender).Content;
-            MessageBox.Show((string)((Button)sender).Content);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
+            MessageBox.Show("Button pressed!");
         }
     }
 }
