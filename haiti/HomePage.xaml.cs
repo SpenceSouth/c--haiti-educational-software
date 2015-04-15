d1e4c15b927a5bf2d029e20efb02dd495d6be431
dinnba <dinnba1@gmail.com>
2015-04-15 09:51:53 -0400

    Navigation between pages now working

---
+++ haiti/HomePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace haiti
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = (string)((Button)sender).Content;
+
+            switch (name)
+            {
+                case "Home":
+                    Uri homeUri = new Uri("HomePage.xaml", UriKind.Relative);
+                    this.NavigationService.Navigate(homeUri);
+                    break;
+                case "About":
+                    Uri aboutUri = new Uri("AboutPage.xaml", UriKind.Relative);
+                    this.NavigationService.Navigate(aboutUri);
+                    break;
+                case "Programs":
+                    Uri programsUri = new Uri("ProgramsPage.xaml", UriKind.Relative);
+                    this.NavigationService.Navigate(programsUri);
+                    break;
+            }            
+
+        }
+    }
+}
