54b4e41035265a1f25e9aa21786bcf1fa7fa81e9
Spence Southard <Spence.Southard@gmail.com>
2015-04-16 13:14:36 -0400

    Added in some grids to the programs page.  Added Brittany's images. Moved datasheets directory to C:/UNF/datasheets

--- haiti/parser/DataSheetParser.cs
+++ haiti/parser/DataSheetParser.cs
@@ -25,7 +25,7 @@ namespace haiti
         public static void parse(){
 
             //Gets all of the files in the folder to begin parsing
-            string[] listOfFiles = Directory.GetFiles(@"C:\\Users\\Spencer\\Documents\\Java Projects\\haiti-educational-software\\datasheets");
+            string[] listOfFiles = Directory.GetFiles(@"C:\\UNF\\datasheets");
 
             foreach(string file in listOfFiles){
                 Category category = extrapolateData(file);
