58eeb6b1ba26c4157274ca78d8e590ac7e7de768
Spence Southard <Spence.Southard@gmail.com>
2015-04-24 08:11:21 -0400

    Added new program class

---
+++ haiti/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace haiti
+{
+    class Program
+    {
+
+        //Decs
+
+        private Program()
+        {
+
+        }
+
+        private static void start(String url, String installation){
+
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                var dr = MessageBox.Show("Sure", "Some Title", MessageBoxButton.YesNo);
+
+                if (dr == MessageBoxResult.Yes)
+                {
+                    Process.Start(installation);
+                }
+                else
+                {
+                    //Do Nothing
+                }
+            }
+        }
+
+        public static void runOlliwitAddition()
+        {
+            Process.Start("C:\\Olltwit\\Addition\\addition.exe");
+        }
+
+    }
+}
--- haiti/level_one/Math_Level_One.xaml.cs
+++ haiti/level_one/Math_Level_One.xaml.cs
@@ -56,6 +56,41 @@ namespace haiti.level_one
 
         }
 
+        private void addImageToButton(Button b, String src)
+        {
+
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(src, UriKind.RelativeOrAbsolute));
+
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.Margin = new Thickness(10);
+            stackPnl.Children.Add(img);
+
+            b.Content = stackPnl;
+            b.Background = Brushes.White;
+
+        }
+
+        private void addImageToButton(Button b, String src, String text)
+        {
+
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(src, UriKind.RelativeOrAbsolute));
+
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.Margin = new Thickness(10);
+            stackPnl.Children.Add(img);
+            TextBlock t = new TextBlock();
+            t.Text = text;
+            //stackPnl.Children.Add(t);
+
+            b.Content = stackPnl;
+            b.Background = Brushes.White;
+
+        }
+
         private void Program_Click(object sender, RoutedEventArgs e)
         {
             string name = (string)((Button)sender).Content;
