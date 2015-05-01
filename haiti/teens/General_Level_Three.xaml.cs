8ecde69a589a1af334af4e462fa813025227847f
dinnba <dinnba1@gmail.com>
2015-05-01 12:04:51 -0400

    Teens general knowledge level 3

--- haiti/teens/General_Level_Three.xaml.cs
+++ haiti/teens/General_Level_Three.xaml.cs
@@ -63,103 +63,124 @@ namespace haiti.teens
             switch (name)
             {
                 case "button0":
-                    Process.Start("teens\\level_3\\GK\\akidsencyclopediaofpoliticalscience.pdf");
+                    if(Utils.Prompt("Description", "100 page encyclopedia on politics.\nLearn about different terms used in science along with meaning, definition and pictures.", 0))
+                        Process.Start("teens\\level_3\\GK\\akidsencyclopediaofpoliticalscience.pdf");
                     break;
                 case "button1":
-                    Process.Start("teens\\level_3\\GK\\childrensillustrateddictionary.pdf");
+                    if(Utils.Prompt("Description", "258 page encyclopedia dictionary with pictures, definitons, and meanings on various subjects like robots, animals, skeleton, universe, music, sports and more.", 0))
+                        Process.Start("teens\\level_3\\GK\\childrensillustrateddictionary.pdf");
                     break;
                 case "button2":
-                    Process.Start("teens\\level_3\\GK\\Climate.ppt");
+                    if(Utils.Prompt("Description", "Causes of climate, winds, location, precipitation, deserts, mountain ranges - 28 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Climate.ppt");
                     break;
                 case "button3":
-                    Process.Start("teens\\level_3\\GK\\Climate__Factors__definition.ppt");
+                    if (Utils.Prompt("Description", "Factors influencing climate; weather versus climate; latitude, wind currents, ocean currents, El-Nino, Greenhouse Effect, more - 11 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Climate__Factors__definition.ppt");
                     break;
                 case "button4":
-                    Process.Start("teens\\level_3\\GK\\completeflagsoftheworldsmithsonianhandbooks.pdf");
+                    if (Utils.Prompt("Description", "248 page book on flags, types of flags, country flags and history of flags.", 0))
+                        Process.Start("teens\\level_3\\GK\\completeflagsoftheworldsmithsonianhandbooks.pdf");
                     break;
                 case "button5":
-                    Process.Start("teens\\level_3\\GK\\Continents.doc");
+                    if (Utils.Prompt("Description", "Slides and outlines of continents - 15 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Continents.doc");
                     break;
                 case "button6":
-                    Process.Start("teens\\level_3\\GK\\Continents__Ocean.ppt");
+                    if (Utils.Prompt("Description", "Seven continents, hemipsheres, exercises - 31 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Continents__Ocean.ppt");
                     break;
                 case "button7":
-                    Process.Start("teens\\level_3\\GK\\countries.pdf");
+                    if (Utils.Prompt("Description", "Country outlines, flags, spelling, and famous people/country icons - 12 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\countries.pdf");
                     break;
                 case "button8":
-                    Process.Start("teens\\level_3\\GK\\Deserts.ppt");
+                    if (Utils.Prompt("Description", "Definitions, types of deserts, desert climate, plants in the desert - 12 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Deserts.ppt");
                     break;
                 case "button9":
-                    Process.Start("teens\\level_3\\GK\\everythingonearth.pdf");
+                    if (Utils.Prompt("Description", "258 page book on everything about Earth, history of Earth, atmosphere, volcanoes, rivers, earthquakes, weather and more.", 0))
+                        Process.Start("teens\\level_3\\GK\\everythingonearth.pdf");
                     break;
                 case "button10":
-                    Process.Start("teens\\level_3\\GK\\Flags_Europe.pdf");
+                    if (Utils.Prompt("Description", "Flags and spelling of country on each slide - 45 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Flags_Europe.pdf");
                     break;
                 case "button11":
-                    Process.Start("teens\\level_3\\GK\\Floods.ppt");
+                    if (Utils.Prompt("Description", "Floods; natural causes, man-made causes, consequences and examples - 9 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Floods.ppt");
                     break;
                 case "button12":
-                    Process.Start("teens\\level_3\\GK\\Gravity_and_Friction.ppt");
+                    if (Utils.Prompt("Description", " Friction and Gravity; Weight;  Air Resistance; Exercises;  Velocity, Formulas, Types of Friction - 17 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Gravity_and_Friction.ppt");
                     break;
                 case "button13":
-                    Process.Start("teens\\level_3\\GK\\humanbodysystemsforkids.pdf");
+                    if (Utils.Prompt("Description", "12 page book for kids on the human body systems and skeleton", 0))
+                        Process.Start("teens\\level_3\\GK\\humanbodysystemsforkids.pdf");
                     break;
                 case "button14":
-                    Process.Start("teens\\level_3\\GK\\justenoughenglishgrammarillustrated.pdf");
+                    if (Utils.Prompt("Description", "145 page book on English grammar with examples and pictures.", 0))
+                        Process.Start("teens\\level_3\\GK\\justenoughenglishgrammarillustrated.pdf");
                     break;
                 case "button15":
-                    Process.Start("teens\\level_3\\GK\\Landforms_.ppt");
+                    if (Utils.Prompt("Description", "Major landforms: rivers, lakes, oceans, mountains, hills, valleys, plains, peninsulas, islands, icecaps - 15 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Landforms_.ppt");
                     break;
                 case "button16":
-                    Process.Start("teens\\level_3\\GK\\Landforms_for_Kids_types.ppt");
+                    if (Utils.Prompt("Description", "Hills versus mountains, valleys, waterfalls, oceans, lakes, isthmus, straits - 17 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Landforms_for_Kids_types.ppt");
                     break;
                 case "button17":
-                    Process.Start("teens\\level_3\\GK\\lyoungchildrenspicturedictionary.pdf");
+                    if (Utils.Prompt("Description", "Dictionary for kids with alphabets, pictures, spellings and word meanings.", 0))
+                        Process.Start("teens\\level_3\\GK\\lyoungchildrenspicturedictionary.pdf");
                     break;
                 case "button18":
-                    Process.Start("teens\\level_3\\GK\\Mountains.ppt");
+                    if (Utils.Prompt("Description", "Definition of mountains, famous mountains and their locations, temperatures, glaciers, climbing mountains, mount everest, aconcagua(Andes), more - 27 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Mountains.ppt");
                     break;
                 case "button19":
-                    Process.Start("teens\\level_3\\GK\\my-new-words-activity-book.pdf");
+                    if (Utils.Prompt("Description", "185 page activity books for children from grade pre-K to 2 with over a 100 activities and games to learn alphabets, numbers, spellings, counting and more.", 0))
+                        Process.Start("teens\\level_3\\GK\\my-new-words-activity-book.pdf");
                     break;
                 case "button20":
-                    Process.Start("teens\\level_3\\GK\\north & south america.pdf");
+                    if (Utils.Prompt("Description", "Flags and Names of Countries(North and South America) on Separate Slide - 87 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\north & south america.pdf");
                     break;
                 case "button21":
-                    Process.Start("teens\\level_3\\GK\\Oceans_Facts.ppt");
+                    if (Utils.Prompt("Description", "Fun facts for kids to learn about the ocean and types of oceans - 22 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Oceans_Facts.ppt");
                     break;
                 case "button22":
-                    Process.Start("teens\\level_3\\GK\\OXYGEN.ppt");
+                    if (Utils.Prompt("Description", "Simple presentation for kids to learn about oxygen, its characteristics and formation - 9 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\OXYGEN.ppt");
                     break;
                 case "button23":
-                    Process.Start("teens\\level_3\\GK\\picturegrammarforchildrenstarter.pdf");
+                    if (Utils.Prompt("Description", "51 page book for kids to learn about grammar with pictures, examples, and activities. ", 0))
+                        Process.Start("teens\\level_3\\GK\\picturegrammarforchildrenstarter.pdf");
                     break;
                 case "button24":
-                    Process.Start("teens\\level_3\\GK\\Rainbows.ppt");
+                    if (Utils.Prompt("Description", "Presentation on rainbows; colors in a rainbow and how it's formed - 21 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Rainbows.ppt");
                     break;
                 case "button25":
-                    Process.Start("teens\\level_3\\GK\\Save_Environment_Why__How.ppt");
+                    if (Utils.Prompt("Description", "Presentation of how we can save the environment by reusing and recycling daily - 18 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Save_Environment_Why__How.ppt");
                     break;
                 case "button26":
-                    Process.Start("teens\\level_3\\GK\\thegreatbookofquestionsandanswersover1000questionsandanswers.pdf");
+                    if (Utils.Prompt("Description", "202 page book to answer all your questions about the universe, planet earth, world history, art and culture, science and technology, human body, animals and more.", 0))
+                        Process.Start("teens\\level_3\\GK\\thegreatbookofquestionsandanswersover1000questionsandanswers.pdf");
                     break;
                 case "button27":
-                    Process.Start("teens\\level_3\\GK\\thevisualworldatlas-factsandmapsofthecurrentworld.pdf");
+                    if (Utils.Prompt("Description", "180 page atlas with pictures of earth, solar system, landscapes, citiies, continents and more.", 0))
+                        Process.Start("teens\\level_3\\GK\\thevisualworldatlas-factsandmapsofthecurrentworld.pdf");
                     break;
                 case "button28":
-                    Process.Start("teens\\level_3\\GK\\tornadoes.ppt");
+                    if (Utils.Prompt("Description", "Definitions, causes and facts - 10 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\tornadoes.ppt");
                     break;
                 case "button29":
-                    Process.Start("teens\\level_3\\GK\\Tsunami_Safety.ppt");
-                    break;
-                case "button30":
-                    Process.Start("teens\\level_3\\GK\\");
-                    break;
-                case "button31":
-                    Process.Start("teens\\level_3\\GK\\");
-                    break;
-                case "button32":
-                    Process.Start("teens\\level_3\\GK\\");
+                    if (Utils.Prompt("Description", "Definition, causes, Tsunami safety; Tsunami in Japan - 8 slides.", 0))
+                        Process.Start("teens\\level_3\\GK\\Tsunami_Safety.ppt");
                     break;
                 default:
                     break;
